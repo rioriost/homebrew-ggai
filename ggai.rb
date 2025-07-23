771601271fd7b6d4ddaf7d2b910db87c5e816a5f
Rio Fujita <rifujita@microsoft.com>
2025-07-24 08:45:30 +0900

    Dependency Update

--- ggai.rb
+++ ggai.rb
@@ -3,8 +3,8 @@ class Ggai < Formula
 
   desc "Helper for GeoGuessr training using OpenAI"
   homepage "https://github.com/rioriost/homebrew-ggai/"
-  url "https://files.pythonhosted.org/packages/7d/7f/1c499474fafd81038b2657f85464fdb3470e3075e9bfbbf995d2dd13a2cc/ggai-0.4.5.tar.gz"
-  sha256 "3a093972987ff1830167fb4dfd0f4cd65792067974f5ea73cf149cd1a55a9019"
+  url "https://files.pythonhosted.org/packages/dd/39/924e8b924e280672fd501613e5165bc3d20ae1865da59c9c3d26d2e94056/ggai-0.4.6.tar.gz"
+  sha256 "12e096551e79c2f5eae82ad830339a3c3674f4a035873a3a2c4b94d1693d8158"
   license "MIT"
 
   depends_on "python@3.10"
